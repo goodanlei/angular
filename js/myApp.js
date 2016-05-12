8c021886660e946eac9f7e1b9692e3fb7722c931
doodlanlei <122901668@qq.com>
2016-05-12 22:23:40 +0800

    修改

--- js/myApp.js
+++ js/myApp.js
@@ -14,16 +14,19 @@ myApp.directive("hello",function(){
 //数据绑定
 var myNum = angular.module("myNum",[]);
 myNum.controller("myCtrl",["$scope",function($scope){
-	$scope.ctrlFlavor = "hello";
+	$scope.ctrlFlavor = "急啊的罚款塑料袋放进；阿里";
 }]);
+
+//@ 单项（字符串）    = 双向 （父级属性）  & 函数  稍后调用
 myNum.directive("hello",function() {
 	// body...
 	return {
 		restrict : "AE",
 		scope : { flavor :'@' },
-		template : "<div>{{flavor}}</div>"
-		// link : function(scope,element,attrs){
-		// 	scope.flavor = attrs.flavor;
-		// }
+		template : "<div>{{flavor}}</div>",
+//		replace: true
+//		 link : function(scope,element,attrs){
+//		 	scope.flavor = attrs.flavor;
+//		 }
 	}
 });
